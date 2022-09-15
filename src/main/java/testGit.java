1ec965c0abdc6b0a58f11c993daabb1dcdb91687
fh <1043755893@qq.com>
2022-09-15 10:30:27 +0800

    hot-fix2 commit

--- src/main/java/testGit.java
+++ src/main/java/testGit.java
@@ -3,5 +3,6 @@ public class testGit {
         System.out.println("hello,git");
         System.out.println("hello,git2");
         System.out.println("hello,branch");
+        System.out.println("test conflict");
     }
 }
